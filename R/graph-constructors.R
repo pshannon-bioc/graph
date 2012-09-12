c18627750f8fa2c2a4f9ccec636fe11e5ece6add
pshannon-bioc <pshannon@fhcrc.org>
2012-09-12 15:46:01 -0700

    added graphAM ctor, passes Martin's test, have not yet eliminated new calls

--- R/graph-constructors.R
+++ R/graph-constructors.R
@@ -3,4 +3,11 @@
 graphNEL <- function (nodes=character(), edgeL=list(), edgemode='undirected') {
   g <- new ('graphNEL', nodes=nodes, edgeL=edgeL, edgemode=edgemode)
   }
-  
+
+
+graphAM <- function (adjMat=matrix(integer(), 0, 0), edgemode='undirected', values=NA) {
+  if (is.na (values))
+    g <- new ('graphAM', adjMat=adjMat, edgemode=edgemode)
+  else
+    g <- new ('graphAM', adjMat=adjMat, edgemode=edgemode, values=values)
+  }
